54e09095981b74d2db11475dd1662a109ed7d99e
Erich E. Hoover <erich.e.hoover@gmail.com>
2014-04-18 14:05:32 -0600

    server: Convert return of file security masks with generic access mappings. (try 7)

--- server/file.c
+++ server/file.c
@@ -482,6 +482,26 @@ struct security_descriptor *mode_to_sd( mode_t mode, const SID *user, const SID 
     return sd;
 }
 
+/* Convert generic rights into standard access rights */
+static void convert_generic_sd( struct security_descriptor *sd )
+{
+    const ACL *dacl;
+    int present;
+
+    dacl = sd_get_dacl( sd, &present );
+    if (present && dacl)
+    {
+        const ACE_HEADER *ace = (const ACE_HEADER *)(dacl + 1);
+        ULONG i;
+
+        for (i = 0; i < dacl->AceCount; i++, ace = ace_next( ace ))
+        {
+            DWORD *mask = (DWORD *)(ace + 1);
+            *mask = generic_file_map_access( *mask );
+        }
+    }
+}
+
 struct security_descriptor *get_file_sd( struct object *obj, struct fd *fd, mode_t *mode,
                                          uid_t *uid )
 {
@@ -620,6 +640,9 @@ int set_file_sd( struct object *obj, struct fd *fd, mode_t *mode, uid_t *uid,
     new_sd = set_sd_from_token_internal( sd, obj->sd, set_info, current->process->token );
     if (new_sd)
     {
+        /* convert generic rights into standard access rights */
+        convert_generic_sd( new_sd );
+
         if (set_info & OWNER_SECURITY_INFORMATION)
         {
             owner = sd_get_owner( new_sd );
