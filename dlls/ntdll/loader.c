6e1e5d4c9657fcce676c4c578b5be1c62fd8b498
Qian Hong <qhong@codeweavers.com>
2015-09-09 05:31:18 +0800

    ntdll: Initialize mod_name to zero.

--- dlls/ntdll/loader.c
+++ dlls/ntdll/loader.c
@@ -1229,6 +1229,8 @@ static NTSTATUS MODULE_InitDLL( WINE_MODREF *wm, UINT reason, LPVOID lpReserved 
     if (wm->ldr.TlsIndex != -1) call_tls_callbacks( wm->ldr.BaseAddress, reason );
     if (!entry || !(wm->ldr.Flags & LDR_IMAGE_IS_DLL)) return STATUS_SUCCESS;
 
+    memset( mod_name, 0, sizeof(mod_name) );
+
     if (TRACE_ON(relay))
     {
         size_t len = min( wm->ldr.BaseDllName.Length, sizeof(mod_name)-sizeof(WCHAR) );
