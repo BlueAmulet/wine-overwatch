9cb0b9c2378ef050947bfe4b7f5b605b27105252
Sebastian Lackner <sebastian@fds-team.de>
2015-09-27 19:35:05 +0200

    user32: Refresh MDI menus when DefMDIChildProc(WM_SETTEXT) is called.

--- dlls/user32/mdi.c
+++ dlls/user32/mdi.c
@@ -1406,6 +1406,7 @@ LRESULT WINAPI DefMDIChildProcA( HWND hwnd, UINT message,
 	DefWindowProcA(hwnd, message, wParam, lParam);
 	if( ci->hwndChildMaximized == hwnd )
 	    MDI_UpdateFrameText( GetParent(client), client, TRUE, NULL );
+        MDI_RefreshMenu( ci );
         return 1; /* success. FIXME: check text length */
 
     case WM_GETMINMAXINFO:
@@ -1446,6 +1447,7 @@ LRESULT WINAPI DefMDIChildProcW( HWND hwnd, UINT message,
         DefWindowProcW(hwnd, message, wParam, lParam);
         if( ci->hwndChildMaximized == hwnd )
             MDI_UpdateFrameText( GetParent(client), client, TRUE, NULL );
+        MDI_RefreshMenu( ci );
         return 1; /* success. FIXME: check text length */
 
     case WM_GETMINMAXINFO:
