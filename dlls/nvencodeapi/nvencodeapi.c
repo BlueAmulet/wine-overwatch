164e83274e1725c8703f5a1d8a727fccc58d6072
Michael Müller <michael@fds-team.de>
2015-12-20 22:13:24 +0100

    nvencodeapi: Add debian specific paths to native library.

--- dlls/nvencodeapi/nvencodeapi.c
+++ dlls/nvencodeapi/nvencodeapi.c
@@ -324,7 +324,23 @@ NVENCSTATUS WINAPI NvEncodeAPICreateInstance(NV_ENCODE_API_FUNCTION_LIST *functi
 
 static BOOL load_nvencode(void)
 {
-    libnvidia_encode_handle = wine_dlopen("libnvidia-encode.so", RTLD_NOW, NULL, 0);
+    static const char *libname[] =
+    {
+        "libnvidia-encode.so",
+        #ifdef __i386__
+        "/usr/lib/i386-linux-gnu/nvidia/current/libnvidia-encode.so",
+        #elif defined __x86_64__
+        "/usr/lib/x86_64-linux-gnu/nvidia/current/libnvidia-encode.so",
+        #endif
+    };
+    int i;
+
+    for (i = 0; i < sizeof(libname)/sizeof(libname[0]); i++)
+    {
+        libnvidia_encode_handle = wine_dlopen(libname[i], RTLD_NOW, NULL, 0);
+        if (libnvidia_encode_handle) break;
+    }
+
     if (!libnvidia_encode_handle)
     {
         FIXME("Wine cannot find the libnvidia-encode.so library, NVIDIA video encoding support disabled.\n");
