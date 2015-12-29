5bb645ed1b8f9dbf927020e574cc0e6ca8087f0f
Alex Henrie <alexhenrie24@gmail.com>
2015-12-29 00:48:02 -0700

    ntdll: Do a device check before returning a default serial port name.
    Fixes https://bugs.winehq.org/show_bug.cgi?id=39793
    
    Signed-off-by: Alex Henrie <alexhenrie24@gmail.com>

--- dlls/ntdll/directory.c
+++ dlls/ntdll/directory.c
@@ -91,6 +91,9 @@
 #ifdef HAVE_SYS_STATFS_H
 #include <sys/statfs.h>
 #endif
+#ifdef HAVE_TERMIOS_H
+# include <termios.h>
+#endif
 #include <time.h>
 #ifdef HAVE_UNISTD_H
 # include <unistd.h>
@@ -433,6 +436,24 @@ static void flush_dir_queue(void)
     }
 }
 
+#ifdef linux
+/* Serial port device files almost always exist on Linux even if the corresponding serial
+ * ports don't exist. Do a basic functionality check before advertising a serial port. */
+static BOOL is_serial_device( const char *unix_name )
+{
+    struct termios tios;
+    BOOL ret = FALSE;
+    int fd;
+
+    if ((fd = open( unix_name, O_RDONLY )) != -1)
+    {
+        ret = tcgetattr( fd, &tios ) != -1;
+        close( fd );
+    }
+
+    return ret;
+}
+#endif
 
 /***********************************************************************
  *           get_default_com_device
@@ -448,6 +469,11 @@ static char *get_default_com_device( int num )
     ret = RtlAllocateHeap( GetProcessHeap(), 0, sizeof("/dev/ttyS256") );
     if (!ret) return NULL;
     sprintf( ret, "/dev/ttyS%d", num - 1 );
+    if (!is_serial_device( ret ))
+    {
+        RtlFreeHeap( GetProcessHeap(), 0, ret );
+        ret = NULL;
+    }
 #elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
     ret = RtlAllocateHeap( GetProcessHeap(), 0, sizeof("/dev/cuau256") );
     if (!ret) return NULL;
