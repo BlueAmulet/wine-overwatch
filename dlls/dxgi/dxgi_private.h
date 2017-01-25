PigFlyGames/wine

dxgi: Improve stubs for MakeWindowAssociation and GetWindowAssociation.

dlls/dxgi/dxgi_private.h
@@ -84,6 +84,7 @@ struct dxgi_factory
     UINT adapter_count;
     IWineDXGIAdapter **adapters;
     BOOL extended;
+    HWND window;
 };
 
 HRESULT dxgi_factory_create(REFIID riid, void **factory, BOOL extended) DECLSPEC_HIDDEN;

dlls/dxgi/factory.c
@@ -161,16 +161,28 @@ static HRESULT STDMETHODCALLTYPE dxgi_factory_EnumAdapters(IWineDXGIFactory *ifa
 
 static HRESULT STDMETHODCALLTYPE dxgi_factory_MakeWindowAssociation(IWineDXGIFactory *iface, HWND window, UINT flags)
 {
+    struct dxgi_factory *factory = impl_from_IWineDXGIFactory(iface);
+
     FIXME("iface %p, window %p, flags %#x stub!\n", iface, window, flags);
 
-    return E_NOTIMPL;
+    if (!window && flags)
+        return DXGI_ERROR_INVALID_CALL;
+
+    factory->window = window;
+    return S_OK;
 }
 
 static HRESULT STDMETHODCALLTYPE dxgi_factory_GetWindowAssociation(IWineDXGIFactory *iface, HWND *window)
 {
+    struct dxgi_factory *factory = impl_from_IWineDXGIFactory(iface);
+
     FIXME("iface %p, window %p stub!\n", iface, window);
 
-    return E_NOTIMPL;
+    if (!window)
+        return DXGI_ERROR_INVALID_CALL;
+
+    *window = factory->window;
+    return S_OK;
 }
 
 static UINT dxgi_rational_to_uint(const DXGI_RATIONAL *rational)

BlueAmulet/wine-overwatch

dlls/dxgi/dxgi_private.h
@@ -109,6 +109,7 @@ struct dxgi_factory
     struct wined3d *wined3d;
     BOOL extended;
     HWND device_window;
+    HWND assoc_window;
 };
 
 HRESULT dxgi_factory_create(REFIID riid, void **factory, BOOL extended) DECLSPEC_HIDDEN;

dlls/dxgi/factory.c
@@ -168,16 +168,28 @@ static HRESULT STDMETHODCALLTYPE dxgi_factory_EnumAdapters(IDXGIFactory1 *iface,
 
 static HRESULT STDMETHODCALLTYPE dxgi_factory_MakeWindowAssociation(IDXGIFactory1 *iface, HWND window, UINT flags)
 {
+    struct dxgi_factory *factory = impl_from_IDXGIFactory1(iface);
+
     FIXME("iface %p, window %p, flags %#x stub!\n", iface, window, flags);
 
+    if (!window && flags)
+        return DXGI_ERROR_INVALID_CALL;
+
+    factory->assoc_window = window;
     return S_OK;
 }
 
 static HRESULT STDMETHODCALLTYPE dxgi_factory_GetWindowAssociation(IDXGIFactory1 *iface, HWND *window)
 {
+    struct dxgi_factory *factory = impl_from_IDXGIFactory1(iface);
+
     FIXME("iface %p, window %p stub!\n", iface, window);
 
-    return E_NOTIMPL;
+    if (!window)
+        return DXGI_ERROR_INVALID_CALL;
+
+    *window = factory->assoc_window;
+    return S_OK;
 }
 
 static HRESULT STDMETHODCALLTYPE dxgi_factory_CreateSwapChain(IDXGIFactory1 *iface,